d1a1e87dcac40130951e4c5a65b565e3ed4d66de
meliza <201913306k@utea.edu.pe>
2022-12-24 15:51:40 -0500

    commit funcion modal

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
 
-$pattern="/[0-9]{2}-[0-9]{4}/";
+$pattern = "/[0-9]{2}-[0-9]{4}/";
 
-if(isset($_GET['month'])&&preg_match($pattern,$_GET['month']))
-{
-    $montharr=explode('-',$_GET['month']);
-    $month=$montharr[0];
-    $year=$montharr[1];
-}
-else
-{
+if (isset($_GET['month']) && preg_match($pattern, $_GET['month'])) {
+    $montharr = explode('-', $_GET['month']);
+    $month = $montharr[0];
+    $year = $montharr[1];
+} else {
     $month = date('m');
     $year = date('Y');
 }
@@ -20,13 +17,10 @@ $firstweekday = date('w', $firstday);
 
 $monthdays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
-if($month==1)
-{
+if ($month == 1) {
     $prevmonth = 12;
-    $prevyear = $year - 1;   
-}
-else
-{
+    $prevyear = $year - 1;
+} else {
     $prevmonth = $month - 1;
     $prevyear = $year;
 }
@@ -34,9 +28,7 @@ else
 if ($month = 12) {
     $nexmonth = 1;
     $nextyear = $year + 1;
-} 
-else 
-{
+} else {
     $nexmonth = $month + 1;
     $nextyear = $year;
 }
@@ -45,37 +37,44 @@ $prevmonthdays = cal_days_in_month(CAL_GREGORIAN, $prevmonth, $prevyear);
 $startweekday = $prevmonthdays - $firstweekday + 1;
 $weekcount = 1;
 $daycount = 1;
-$nextday = 1;    
+$nextday = 1;
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ToDoList</title>    
-  </head>
-    <style>
-        @import url('css/bootstrap.css');
-        @import url('css/fontello.css');
-        @import url('css/bootstrap-datepicker.css');
-        @import url('css/style.css');
-    </style> 
-    <script src="js/jquery-3.6.3.min.js"></script>
-    <script src="js/bootstrap-datepicker.js"></script>
-    <script>
-        $(function()
-        {
-            $('.datepicker').datepicker({
-                format: "mm-yyyy",
-                startView: 1,
-                minViewMode: 1
-            });
-        })     
-
-    </script>
+    <title>ToDoList</title>
 </head>
+<style>
+    @import url('css/bootstrap.css');
+    @import url('css/fontello.css');
+    @import url('css/bootstrap-datepicker.css');
+    @import url('css/style.css');
+</style>
+
+<script src="js/jquery-3.6.3.min.js"></script>
+<script src="js/bootstrap.js"></script> 
+<script src="js/bootstrap-datepicker.js"></script>
+<script>
+    
+    $(function() {
+        $('.datepicker').datepicker({
+            format: "mm-yyyy",
+            startView: 1,
+            minViewMode: 1
+        });
+
+        $('.btn-dark').on('click', function(){
+          $('#exampleModal').modal('show');
+        });
+    });
+</script>
+</head>
+
 <body>
     <div class="container">
         <h3><i class="icon-calendar"></i>To Do List</h3>
@@ -92,11 +91,11 @@ $nextday = 1;
                         </div>
                     </div>
                 </form>
-            </div> 
+            </div>
             <div class="col-md-8">
-                <h4 class="float-end"><?="$monthname $year"?></h4>
-            </div>        
-        </div>        
+                <h4 class="float-end"><?= "$monthname $year" ?></h4>
+            </div>
+        </div>
         <!--formulario-->
         <!--calendario-->
         <table class="table">
@@ -107,28 +106,24 @@ $nextday = 1;
                 <th>Miercoles</th>
                 <th>Jueves</th>
                 <th>Viernes</th>
-                <th>Sabado</th>                
+                <th>Sabado</th>
             </tr>
             <tr>
                 <?php
-                while($firstweekday>0)
-                {
+                while ($firstweekday > 0) {
                     echo '<td class="text-muted">' . $startweekday++ . '</td>';
                     $firstweekday--;
                     $weekcount++;
-                }               
-                while($daycount<=$monthdays)
-                {
-                    echo '<td>' . $daycount++ . '</td>';
+                }
+                while ($daycount <= $monthdays) {
+                    echo '<td><butoon data-date="' . $year . '-' . $month . '-' . $daycount . '" class="btn btn-sm btn-dark">' . $daycount++ . '</butoon></td>';
                     $weekcount++;
-                    if($weekcount>7)
-                    {
-                        echo'</tr><tr>';
+                    if ($weekcount > 7) {
+                        echo '</tr><tr>';
                         $weekcount = 1;
                     }
                 }
-                while($weekcount>1 && $weekcount<=7)
-                {
+                while ($weekcount > 1 && $weekcount <= 7) {
                     echo '<td class="text-muted">' . $nextday++ . '</td>';
                     $weekcount++;
                 }
@@ -137,5 +132,25 @@ $nextday = 1;
         </table>
         <!--calendario-->
     </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
