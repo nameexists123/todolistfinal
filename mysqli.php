b2dd51f1ded4e609e7fe3782e3783ef6dbe63a6d
meliza <201913306k@utea.edu.pe>
2022-12-24 20:21:24 -0500

    commit mejorar las funciones del modal y recuperar fecha

---
+++ mysqli.php
@@ -0,0 +1,10 @@
+<?php
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$dataBase = 'bdtodolistfinal';
+
+
+$mysqli =mysqli_connect($server,$user,$password,$dataBase)or die(mysqli_connect_error());
+
+?>
