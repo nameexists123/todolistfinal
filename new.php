c7637d6b339444ced6894e8d170a8ebca02e8b0d
meliza <201913306k@utea.edu.pe>
2022-12-24 21:04:49 -0500

    commit ingresar datos y verificarlos en la base de datos

---
+++ new.php
@@ -0,0 +1,20 @@
+
+    <?php
+    /* [fecha] => 2022-12-1
+    [hora] => 21:33
+    [categorias] => 1
+    [nombre] => clasica */
+    require 'mysqli.php';
+    $date = $mysqli->escape_string(strip_tags($_POST['date']) );
+    $time = $mysqli->escape_string(strip_tags($_POST['time']));
+    $category= $mysqli->escape_string(strip_tags($_POST['categorias']));
+    $name = $mysqli->escape_string(strip_tags($_POST['name']));
+
+    $fixedDate = date('Y-m-d h:i:s', strtotime($date.' '.$time));
+
+    $query = "INSERT INTO tareas (categoria,nombre,fecha) VALUES ($category,'$name','$fixedDate')";
+
+    $mysqli->query($query)or die($mysqli->error);
+    $url= date('m-Y',strtotime($date.''.$time));
+    header('Location: index.php?month='.$url);
+    ?>
