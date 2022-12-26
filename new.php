5a733739ac6cfdfa7ed390eb34b4a25219273956
meliza <201913306k@utea.edu.pe>
2022-12-26 15:03:52 -0500

    commit mostrando eventos en el calendario

--- new.php
+++ new.php
@@ -5,14 +5,14 @@
     [categorias] => 1
     [nombre] => clasica */
     require 'mysqli.php';
-    $date = $mysqli->escape_string(strip_tags($_POST['date']) );
+    $date = $mysqli->escape_string(strip_tags($_POST['date']));
     $time = $mysqli->escape_string(strip_tags($_POST['time']));
     $category= $mysqli->escape_string(strip_tags($_POST['category']));
     $name = $mysqli->escape_string(strip_tags($_POST['name']));
 
     $fixedDate = date('Y-m-d h:i:s', strtotime($date.' '.$time));
 
-    $query = "INSERT INTO events (cat, name, date) VALUES ($category,'$name','$fixedDate')";
+    $query = "INSERT INTO events (cat,name, date) VALUES ('$category','$name','$fixedDate')";
 
     $mysqli->query($query)or die($mysqli->error);
 
